7e64343408870e820e808defe34df07f2ad41740
MariangelB <mariamgelbarrios5@gmail.com>
2025-10-27 02:06:16 -0600

    Cambios recursividad

---
+++ CP1/Recursividad/Numbers.java
@@ -0,0 +1,62 @@
+public class Numbers {
+
+    private static double N = 25;
+
+    public static double formula(double z) {
+        return round((z + Math.sqrt(4 + Math.pow(z, 2))) / 2);
+    }
+
+    public static double recursive(double z) {
+        return round(recursive(z, N) / recursive(z, N - 1));
+    }
+
+    public static double iterative(double z) {
+        return round(iterative(z, N) / iterative(z, N - 1));
+    }
+
+    private static double recursive(double z, double n) {
+        return n == 0 || n == 1 ? 1 : z * recursive(z, n - 1) + recursive(z, n - 2);
+    }//Validando la formula, estaba incompleta
+
+    private static double iterative(double z, double n) {//Gemini
+        if (n <= 1) {
+            return 1;
+        }
+
+        double prev2 = 1; 
+        double prev1 = 1; 
+
+        for (int i = 2; i <= n; i++) {
+            double current = z * prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1; 
+    }
+
+    private static double round(double value) {//error #1
+        var ROUND = 10000000000.0;
+        return Math.round(value * ROUND) / ROUND;
+    }
+
+    public static void main(String[] args) {
+        String[] metallics = {
+                "Platinum", // [0]
+                "Golden", // [1]
+                "Silver", // [2]
+                "Bronze", // [3]
+                "Copper", // [4]
+                "Nickel", // [5]
+                "Aluminum", // [6]
+                "Iron", // [7]
+                "Tin", // [8]
+                "Lead", // [9]
+        };
+        for (var z = 0; z < metallics.length; z++) {
+            System.out.println("\n[" + z + "] " + metallics[z]);
+            System.out.println(" ↳ formula(" + z + ")   ≈ " + formula(z));
+            System.out.println(" ↳ recursive(" + z + ") ≈ " + recursive(z));
+            System.out.println(" ↳ iterative(" + z + ") ≈ " + iterative(z));
+        }
+    }
+}
